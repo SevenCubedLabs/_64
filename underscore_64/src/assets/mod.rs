78d29654e7fddb90dae362034f834b9629c8effb
Zeno343 <62606540+Zeno343@users.noreply.github.com>
2022-09-08 00:18:46 -0400

    2022-07-17 Release Candidate (#12)
    * Implement binding tuples of vertices (#11)
    
    * refactor: Move vertex into mesh
    
    * feat: Implemented Vertex for tuples of vertices
    
    * refactor: Moved shaders into their own module
    
    * fix: Update curves example to use new shader module
    
    * Editor crate setup (#13)
    
    refactor: Reorganize workspace for editor bin crate

---
+++ underscore_64/src/assets/mod.rs
@@ -0,0 +1,14 @@
+pub mod shaders {
+    macro_rules! shader_src {
+        ($src:literal) => {
+            concat!(include_str!($src), "\0")
+        };
+    }
+
+    pub const POS2D: &str = shader_src!("shaders/pos2d.vert");
+    pub const POS3D: &str = shader_src!("shaders/pos3d.vert");
+    pub const POS2D_RGB: &str = shader_src!("shaders/pos2d_rgb.vert");
+    pub const POS3D_RGB: &str = shader_src!("shaders/pos3d_rgb.vert");
+    pub const WHITE: &str = shader_src!("shaders/white.frag");
+    pub const RGB: &str = shader_src!("shaders/rgb.frag");
+}
--- underscore_64/src/render/buffer.rs
+++ underscore_64/src/render/buffer.rs
@@ -1,4 +1,4 @@
-use _sys::*;
+use underscore_sys::*;
 
 pub struct Buffer {
     _type: GLenum,
